0bd344c4c79f82703041f37a69ac447cf8abc60a
nitin621 <70137363+nitin621@users.noreply.github.com>
2024-01-31 06:05:04 +0000

    variable and dataTypes

---
+++ 01_Basic/Variables.js
@@ -0,0 +1,11 @@
+const officeId = 121
+
+let officeName = 'ICAR'
+
+var officeDivisions = 'one hundred thirteen'
+
+officeSalary = 45000
+
+let OfficeLoction;
+
+console.table([officeId,officeName,officeDivisions,officeSalary,OfficeLoction])
---
+++ 01_Basic/dataTypes.js
@@ -0,0 +1,11 @@
+/********All Are preemitive type data types********/
+// number => 2 to the power 53
+// bigint => For big value like stock marketing
+// string => "Hello Future"
+// boolean => true/false
+// null => standalone value (its type is object)
+// undefined => (its typeis undefined)
+
+// symbol => unique
+
+/************************************************/
