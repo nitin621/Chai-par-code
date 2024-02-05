66c7a66c5c94ae7cda8220ffd6d26bc31e15e08d
nitin621 <70137363+nitin621@users.noreply.github.com>
2024-02-05 07:02:56 +0000

    updated

---
+++ 01_Basic/03_Conversions.js
@@ -0,0 +1,5 @@
+let no = true
+
+let str = String(no)
+
+console.table([typeof str,str])
---
+++ 01_Basic/04_Comparision.js
@@ -0,0 +1,7 @@
+
+console.log(1>2)
+
+
+console.table([null > 0,null ==0, null>=0, null ===0])
+
+console.table([undefined>0, undefined == 0, undefined>=0, undefined === 0])
