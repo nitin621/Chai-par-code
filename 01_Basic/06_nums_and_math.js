34dffa68931ad8d3f33eaeed8e4e0c8c59528e0e
nitin621 <70137363+nitin621@users.noreply.github.com>
2024-02-12 11:40:15 +0000

    Maths Function

---
+++ 01_Basic/06_nums_and_math.js
@@ -0,0 +1,24 @@
+
+
+// const num = new Number(100)
+
+// console.log(num)
+
+// console.log(num.toString().length)
+
+// console.log(num.toFixed(2))
+
+// const OtherNum = 123.8966
+
+// console.log(OtherNum.toPrecision(3))
+
+// const hund = 1000000
+
+// console.log(hund.toLocaleString('en-in'))
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+const Max =100
+const Min = 10
+
+console.log(Math.floor(Math.random()*(Max-Min+1)+Min));
