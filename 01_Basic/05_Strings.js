2988c1182004fa0f97cffc6b9ebf60ec9f55fbe5
nitin621 <70137363+nitin621@users.noreply.github.com>
2024-02-12 07:22:53 +0000

    String

--- 01_Basic/05_Strings.js
+++ 01_Basic/05_Strings.js
@@ -4,6 +4,14 @@ lName = "Sain"
 
 console.log(`His name is ${fName} and his last name is ${lName}`)
 
-const Str = new String("Nitin Sain") 
+const Str = 'Hello world sachin'
 
-console.log(Str.codePointAt(1))
+
+console.log(Str.substring(-3,3))
+
+
+console.log(Str.slice(-8,10))
+
+const Str1 = 'Nitin%Sain*Manikam#Arora'
+
+console.log(Str1.split(/[% * #]+/))
